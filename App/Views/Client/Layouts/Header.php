20707247b631430918cf24be7f0dcde54c0b7805
thuongdtd <156890208+thuongdtd@users.noreply.github.com>
2024-11-14 17:25:57 +0700

    sửa lại ổn footer với thêm link icon cho header

--- App/Views/Client/Layouts/Header.php
+++ App/Views/Client/Layouts/Header.php
@@ -26,6 +26,7 @@ class Header extends BaseView
             <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
             <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+            <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
         </head>
 
         <body>
