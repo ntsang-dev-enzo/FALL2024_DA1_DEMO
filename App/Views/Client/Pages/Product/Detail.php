6752e2e02db88951e72f217def54fd2cfb906495
Nguyễn Trung Sang <sangntpc08899@gmail.com>
2024-11-21 17:51:15 +0700

    Detail

--- App/Views/Client/Pages/Product/Detail.php
+++ App/Views/Client/Pages/Product/Detail.php
@@ -105,7 +105,7 @@ class Detail extends BaseView
                     </div>
 
                 </div>
-            </div>
+            
 
             <!-- Related Products Section -->
 
@@ -215,6 +215,7 @@ class Detail extends BaseView
                     </div>
                 </div>
             </div>
+        </div>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
