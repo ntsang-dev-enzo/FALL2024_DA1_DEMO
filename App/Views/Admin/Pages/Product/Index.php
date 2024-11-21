89524fe417e57d77b95afac20834bda8ff7d2a8e
thuongdtd <156890208+thuongdtd@users.noreply.github.com>
2024-11-21 16:41:20 +0700

    admin_Thêm sản phẩm_sửa xóa_danh sách

--- App/Views/Admin/Pages/Product/Index.php
+++ App/Views/Admin/Pages/Product/Index.php
@@ -75,7 +75,7 @@ class Index extends BaseView
                                                         <td><?= ($item['status'] == 1) ? 'Hiển thị' : 'Ẩn' ?></td>
                                                         <td>
                                                             <a href="/admin/products/<?= $item['id'] ?>" class="btn btn-primary ">Sửa</a>
-                                                            <form action="/admin/products/<?= $item['id'] ?>" method="post" style="display: inline-block;" onsubmit="return confirm('Bạn chắc chắn muốn xóa?')">
+                                                            <form action="/admin/products/<?= $item['id'] ?>" method="post" style="display: inline-block;" onsubmit="return confirm('Bạn chắc chắn muốn xóa sản phẩm <?= $item['productname'] ?> không?')">
                                                                 <input type="hidden" name="method" value="DELETE" id="">
                                                                 <button type="submit" class="btn btn-danger text-white">Xoá</button>
                                                             </form>
